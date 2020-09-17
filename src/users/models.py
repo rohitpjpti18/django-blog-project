bd2194647a5bedc232ceca437e3e70c4f30147cd
Rohit Prajapati <51499109+rohitpjpti18@users.noreply.github.com>
2020-09-17 17:58:08 +0530

    reorganized project for presentation

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,3 +9,34 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
+
+
+	
+	def save(self, *args, **kwargs):
+		super().save( *args, **kwargs)
+
+		img = Image.open(self.image.path)
+		width, height = img.size
+		#print(width, height)
+
+		# check which one is smaller
+		if height < width:
+			# make square by cutting off equal amounts left and right
+			left = (width - height) / 2
+			right = (width + height) / 2
+			top = 0
+			bottom = height
+			img = img.crop((left, top, right, bottom))
+			img.thumbnail((300, 300))
+			img.save(self.image.path)
+
+		elif width < height:
+			# make square by cutting from bottom
+			left = 0
+			right = width
+			top = 0
+			bottom = width
+			img = img.crop((left, top, right, bottom))
+			img.thumbnail((300, 300))
+
+		img.save(self.image.path)
